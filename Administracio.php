4a46c248ed0b50922015e237dd232360ae633158
Rickysox <92725365+Rickysox@users.noreply.github.com>
2021-10-18 13:09:19 +0200

    Add files via upload

---
+++ Administracio.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Inici</title>
+</head>
+<body>
+<h1>Pantalla administració</h1>
+<form action="inici.php">
+    <br><br>
+    <input type="submit" name="boton" value="Inici">
+</form>
+<form action="historial.php">
+    <br><br>
+    <input type="submit" name="boton" value="Historial">
+</form>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Inici</title>
+</head>
+<body>
+    <h1>Menu</h1>
+    <form action="comandav.php">
+        <br><br>
+        <input type="submit" name="boton" value="Validació comanda">
+    </form>
+    <form action="inicio.php">
+        <br><br>
+        <input type="submit" name="boton" value="Inicio">
+    </form>
+</body>
+</html>
