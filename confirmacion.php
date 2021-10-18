dd0e4aa0873fa045bd8d2cf8537d967c9b15790e
grefusaa <74738781+grefusaa@users.noreply.github.com>
2021-10-18 13:10:47 +0200

    Add files via upload

---
+++ confirmacion.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Comanda</title>
+</head>
+<body>
+<h1>Comanda</h1>
+
+
+<form action="comandav.php">
+    <br/>
+    <input type="submit" name="button" value="Comanda">
+</form>
+<?php
+echo "Confimación";
+?>
+</body>
+
+
+</html>
+
+
---
+++ inicio.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Inicio</title>
+</head>
+<body>
+<h1>Inicio</h1>
+
+    <form action="menu.php">
+        <br/>
+        <input type="submit" name="button" value="Menú">
+    </form>
+
+</body>
+<?php
+echo "Inicio";
+?>
+
+</html>
