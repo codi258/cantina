65c21f9551eb6f720e281e3b4beafd87f20211da
codi258 <92580158+codi258@users.noreply.github.com>
2021-10-19 10:18:11 +0200

    Add files via upload

---
+++ php/foother.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<style>
+
+    header.page-header {
+        background: no-repeat left/cover url(https://colorazul.top/wp-content/uploads/2020/04/Azul-claro.jpg);
+        display: flex;
+        height: 30px;
+        min-width: 30px;
+        align-items: center;
+        color: #fff;
+        text-shadow: #000 0 0 .2em;
+    }
+
+    header.page-header > h1 {
+        font: bold calc(1em + 2 * (100vw - 120px) / 100) 'Dancing Script', cursive,
+        fantasy;
+        margin: 2%;
+        text-align: center;    }
+
+    main {
+        font: 1rem 'Fira Sans', sans-serif;
+    }
+
+</style>
+<header class="page-header">
+    <h1></h1>
+</header>
+
+
+
+</html>
