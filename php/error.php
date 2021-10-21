825ad69b4321d9383bedb32186dfe4439cc97ccf
Rickysox <92725365+Rickysox@users.noreply.github.com>
2021-10-21 11:54:54 +0200

    hola

--- php/error.php
+++ php/error.php
@@ -6,6 +6,10 @@
     <body>
         <h1>Ya has comprado el dia de hoy!</h1>
     </body>
+    <form action="inicio.php">
+        <br><br>
+        <input type="submit" name="boton" value="Inicio">
+    </form>
     <?php
 
 
