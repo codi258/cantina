fbd76a7a4250067212d500081448925e6b15a68a
Rickysox <92725365+Rickysox@users.noreply.github.com>
2021-10-21 11:48:49 +0200

    ghil

--- php/confirmacion.php
+++ php/confirmacion.php
@@ -1,35 +1,35 @@
 <html>
-<head>
-    <title>Recibo</title>
+    <head>
+        <title>Recibo</title>
 
-</head>
-<body>
-<header>
-    <?php
-     include 'Header.php';
-    ?>
-</header>
-<h1>Tu pedido</h1>
+    </head>
+    <body>
+        <header>
+            <?php
+             include 'Header.php';
+            ?>
+        </header>
+        <h1>Tu pedido</h1>
 
 
-<form action="comandav.php">
-    <br/>
-    <input type="submit" name="button" value="Comanda">
-</form>
-<?php
-echo "Confimación";
-?>
-<form action="inicio.php">
-    <br><br>
-    <input type="submit" name="boton" value="Inicio">
-</form>
-<footer>
-    <?php
-    include 'Footer.php'
+        <form action="comandav.php">
+            <br/>
+            <input type="submit" name="button" value="Comanda">
+        </form>
+        <?php
+        echo "Confimación";
+        ?>
+        <form action="inicio.php">
+            <br><br>
+            <input type="submit" name="boton" value="Inicio">
+        </form>
+        <footer>
+            <?php
+            include 'Footer.php'
 
-    ?>
-</footer>
-</body>
+            ?>
+        </footer>
+    </body>
 </html>
 
 
---
+++ php/error.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/elcss.css">
+        <title>Error!</title>
+    </head>
+    <body>
+        <h1>Ya has comprado el dia de hoy!</h1>
+    </body>
+    <?php
+
+
+
+    ?>
+
+</html>
+
+
+
